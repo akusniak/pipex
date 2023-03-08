3f060f811dec236f0325e5a453d2f471664bbe4c
akusniak <axel.kusniak@gmail.com>
2023-03-08 15:24:41 +0100

    25%d done

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: akusniak <akusniak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 12:52:30 by akusniak          #+#    #+#             */
-/*   Updated: 2023/03/08 13:09:46 by akusniak         ###   ########.fr       */
+/*   Updated: 2023/03/08 15:23:01 by akusniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,120 +18,59 @@
 
 #include <stdio.h>//debug
 
-// int main()
-// {
-// 	int	id;
+//        ./pipex file1 cmd1 cmd2 file2
 
-// 	id = fork(); //--> return un int
-// 	if (id == 0)// --> si fork retourne 0 alors on se trouve dans le child
-// 	{
-// 		printf("Hello from child [%d]", id);
-// 	}
-// 	else// --> sinon on se trouve dans le main process
-// 	{
-// 		printf("Hello from parent [%d]", id);
-// 		fork();
-// 	}
-// 	return (0);
-// }
-
-
-// int main()// --> result impredictable without wait();
-// {
-// 	int id;
-// 	int	n;
-
-// 	id = fork();
-// 	if (id == 0)
-// 	{
-// 		n = 1;
-// 	}
-// 	else
-// 	{
-// 		n = 6;
-// 	}
-// 	if (id != 0)
-// 	{
-// 		wait(NULL); //NULL --> wait renvoie une valeur
-// 	}
-// 	int i;
-// 	for (i = n; i < n + 5; i++){
-// 		printf("%d ", i);
-// 		fflush(stdout);
-// 	}
-// 	return (0);
-// }
-
-// int	main()
-// {
-// 	int	id;
-
-// 	id = fork();
-// 	printf("[%d][%d]", getpid(), getppid());
-// 	return (0);
-// }
-
-// int main()
-// {
-// 	int	fd[2];
-// 	// fd[0] - read
-// 	// fd[1] - write
-// 	int	id;
-
-// 	pipe(fd);
-// 	id = fork();
-// 	if (id == 0)
-// 	{
-// 		int	x;
-
-// 		close(fd[0]);
-// 		printf("Input a number : ");
-// 		scanf("%d", &x);
-// 		write(fd[1], &x, sizeof(int));
-// 		close(fd[1]);
-// 	}
-// 	else
-// 	{
-// 		int	y;
-
-// 		close(fd[1]);
-// 		read(fd[0], &y, sizeof(int));
-// 		close(fd[0]);
-// 		printf("Got from child : %d", y);//je suis dans le parent process
-// 	}
-// 	return (0);
-// }
-
-int main()
+int main()//int ac, char **param)
 {
-	int	fd[2];
-	int	pid1;
-	int	pid2;
+	int	fd[2]; //0 - read **** 1 - write
+	pid_t	pid1;
+	pid_t	pid2;
+	int		fd1;
+	int		fd2;
 
-	if (pipe(fd) == -1)
-		return (1);
-	pid1 = fork();
-	if (pid1 < 0)
-		return (1);
-	if (pid1 == 0)// on va effectuer nos actions dans ce child process
-	{
-		dup2(fd[1], STDOUT_FILENO);// remains open even if we dup it
+	/*check les prametres*/
+	// if (ac == 5)
+	// {
+		//(void)param;
+		if (pipe(fd) == -1)
+			return (1);
+		pid1 = fork();
+		if (pid1 < 0)
+			return (1);
+		fd1 = open("axel.txt", O_RDONLY);
+		if (pid1 == 0)//on se trouve dans le child process
+		{
+			dup2(fd1, STDIN_FILENO); // on fait pointer la tete de lecture [0] vers le fichier qu'on souhaite lire
+			dup2(fd[1], STDOUT_FILENO);
+			close(fd[0]);
+			close (fd1);
+			//close(fd[1]);
+			execlp("cat", "cat", NULL);// execution de notre premiere commande
+		}
+		fd2 = open("result.txt", O_CREAT | O_RDWR, 00700);
+		pid2 = fork();
+		if (pid2 == 0)
+		{
+			dup2(fd[0], STDIN_FILENO);
+			dup2(fd2, STDOUT_FILENO);
+			close(fd[1]);
+			close(fd2);
+			execlp("ls", "ls", NULL);
+		}
+		//close(fd1);
+		//close(fd2);
 		close(fd[0]);
-		close(fd[1]);
-		execlp("ping", "ping", "-c", "3", "google.com", NULL);// execution de notre premiere commande
-	}
-	pid2 = fork();
-	if (pid2 == 0)
-	{
-		dup2(fd[0], STDIN_FILENO);
-		close(fd[1]);
-		close(fd[0]);
-		execlp("grep", "grep", "rtt", NULL);
-	}
-	close(fd[0]);
-	close(fd[1]);//close avant les waitpid
-	waitpid(pid1, NULL, 0);// pour laisserle teps au child de s'executer
-	waitpid(pid2, NULL, 0);// pour laisserle teps au child de s'executer
-	return (0);
+		close(fd[1]);//close avant les waitpid
+		waitpid(pid1, NULL, 0);// pour laisserle teps au child de s'executer
+		waitpid(pid2, NULL, 0);// pour laisserle teps au child de s'executer
+		return (0);
+	//}
+	//else
+	//{
+		//ft_printf("Usage : .pipex file1 command1 command2 file2\n");
+		//return (1);
+	//}
 }
 
+// .pipex   file1    command1 command2 file2
+// param[0] param[1] param[2] param[3] param[4]
